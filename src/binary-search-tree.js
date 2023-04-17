f9093bdee7cc5564ab34a519db929a895a88245e
1zemzem <1zemliakova@gmail.com>
2023-04-17 15:20:31 +0300

    binary-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,118 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require("../extensions/list-tree.js");
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+  constructor() {
+    this.tree = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.tree;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    if (this.tree === null) {
+      this.tree = new Node(data);
+      return;
+    }
+    let curr = this.tree;
+    while (curr) {
+      if (data < curr.data && !curr.left) {
+        curr.left = new Node(data);
+        return;
+      }
+      if (data < curr.data && curr.left) {
+        curr = curr.left;
+      }
+      if (data > curr.data && !curr.right) {
+        curr.right = new Node(data);
+        return;
+      }
+      if (data > curr.data && curr.right) {
+        curr = curr.right;
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let curr = this.tree;
+    while (curr) {
+      if (curr.data === data) return true;
+      if (curr.data > data) {
+        curr = curr.left;
+      } else {
+        curr = curr.right;
+      }
+    }
+    return false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let curr = this.tree;
+    while (curr) {
+      if (curr.data === data) return curr;
+      if (curr.data > data) {
+        curr = curr.left;
+      } else {
+        curr = curr.right;
+      }
+    }
+    return null;
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    if (this.tree === null) return null;
+    this.tree = this.removeNode(this.tree, data);
+  }
+  removeNode(currNode, data) {
+    let isChildren = currNode.left === null && currNode.right === null;
+    if (currNode.data === data) {
+      if (isChildren) return null;
+      if (currNode.left === null) return currNode.right;
+      if (currNode.right === null) return currNode.left;
+
+      let minFromRight = this.searchMin(currNode.right);
+      currNode.data = minFromRight.data;
+      currNode.right = this.removeNode(currNode.right, minFromRight.data);
+      return currNode;
+    }
+    if (data < currNode.data && currNode.left !== null) {
+      currNode.left = this.removeNode(currNode.left, data);
+    } else if (data > currNode.data && currNode.right !== null) {
+      currNode.right = this.removeNode(currNode.right, data);
+    }
+    return currNode;
+  }
+  searchMin(node) {
+    if (node.left !== null) return this.searchMin(node.left);
+    return node;
+
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.tree === null) return null;
+    let curr = this.tree;
+    while (curr.left) {
+      curr = curr.left;
+    }
+    return curr.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.tree === null) return null;
+    let curr = this.tree;
+    while (curr.right) {
+      curr = curr.right;
+    }
+    return curr.data;
   }
 }
 
 module.exports = {
-  BinarySearchTree
+  BinarySearchTree,
 };
